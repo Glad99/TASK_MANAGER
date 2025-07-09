27aa2e0896b641a16ac4ac29e9cca089b4cfdc3e
Glad99 <gladysalu1@gmail.com>
2025-07-09 21:59:56 +0100

    commit

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -6,5 +6,136 @@ const Task = require("../models/Task");
 const getTasks = async (req, res) => {
     try {
 
-    } catch (error)
-}
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+//  @desc Get a task by ID
+//  @route Get /api/tasks/:id   
+// @access Private
+const getTaskById = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc Create a new task (Admin only)
+// @route Post /api/tasks/      
+// @access Private
+const createTask = async (req, res) => {
+    try {
+        const {
+            title,
+            description,
+            assignedTo,
+            dueDate,
+            priority,
+            attachments,
+            todoChecklist,
+        } = req.body;
+
+        if (!Array.isArray(assignedTo)) {
+            return res.status(400).json({ message: "AssignedTo must be an array of user IDs" });
+        }
+
+        const task = Task.create({
+            title,
+            description,
+            priority,
+            assignedTo, 
+            dueDate,
+            createdBy: req.user._id,
+            attachments,
+            todoChecklist,
+        });
+
+        res.status(201).json({
+            message: "Task created successfully",   
+            task,
+        });  
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc Update a task (Admin only)
+// @route Put /api/tasks/:id
+// @access Private  
+const updateTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc Delete a task (Admin only)
+// @route Delete /api/tasks/:id
+// @access Private
+const deleteTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+//  @desc Update task status (User)
+//  @route Put /api/tasks/:id/status
+//  @access Private
+const updateTaskStatus = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc Update task checklist
+// @route Put /api/tasks/:id/todo
+// @access Private
+const updateTaskChecklist = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc Dashboard Date (Admin only)
+// @route Get /api/tasks/dashboard
+// @access Private  
+const getDashboardData = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc Dashboard Data (User-specific)
+// @route Get /api/tasks/user-dashboard-data
+// @access Private
+const getUserDashboardData = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+module.exports = {
+    getTasks,      
+    getTaskById,
+    createTask,     
+    updateTask,
+    deleteTask,
+    updateTaskStatus,
+    updateTaskChecklist,    
+    getDashboardData,
+    getUserDashboardData,
+};
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middlewares/authMiddleware");
+const { getDashboardData, getUserDashboardData, getTasks, getTaskById, createTask, updateTask, deleteTask, updateTaskStatus, updateTaskChecklist } = require("../controllers/taskController");
 
 const router = express.Router();    
 
@@ -9,9 +10,9 @@ router.get("/user-dashboard", protect, getUserDashboardData);
 router.get("/", protect, getTasks); // Get all tasks(Admin: all, User: assigned)
 router.get("/:id", protect, getTaskById); // Get task by ID
 router.post("/", protect, adminOnly, createTask); // Create a task (Admin only)
-router.put("/:id", protect, adminOnly, UpdateTask); // Update a task details
+router.put("/:id", protect, adminOnly, updateTask); // Update a task details
 router.delete("/:id", protect, adminOnly, deleteTask); // Delete a task (Admin Only)
-router.put("/:id/status", protect, adminOnly, UpdateTaskStatus); // Updates task status
-router.put("/:id/todo", protect, adminOnly, UpdateTaskChecklist); // Updates task Checklist
+router.put("/:id/status", protect, adminOnly, updateTaskStatus); // Updates task status
+router.put("/:id/todo", protect, adminOnly, updateTaskChecklist); // Updates task Checklist
 
 module.exports = router;

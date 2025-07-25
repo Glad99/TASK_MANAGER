a826df9f04f1fae1d32e9a044ce920380882c928
Glad99 <gladysalu1@gmail.com>
2025-07-25 23:43:21 +0100

    commit

--- backend/routes/reportsRoutes.js
+++ backend/routes/reportsRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const { adminOnly, protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.get("/report/task") 
+router.get("/report/tasks", protect, adminOnly, exportTasksReport); //Export all tasks as Excel/PDF
+router.get("/export/users", protect, adminOnly, exportUsersReport); //Export all user-task report
+
+module.exports = router;

e62e4f4d36733e9b4ecae42fb5129b99beb61a30
Glad99 <gladysalu1@gmail.com>
2025-07-25 23:51:49 +0100

    commit

---
+++ backend/controllers/reportsController.js
@@ -0,0 +1,28 @@
+const Task = require("../models/Task");
+const User = require("../models/User");
+const excelJS = require("exceljs");
+
+
+// @desc Export all tasks as an Excel file
+// @route GET /api/reports/tasks
+// @access Private/Admin
+const exportTasksReport = async (req, res) => { 
+    try{
+
+    } catch (error) { 
+        res
+            .status(500)
+            .json({ message: "Failed to export tasks report", error: error.message });
+    }
+};
+
+// @desc Export all user-task report as an Excel file
+// @route GET /api/reports/export/users
+// @access Private/Admin 
+const exportUsersReport = async (req, res) => {};
+
+module.exports = {
+    exportTasksReport,
+    exportUsersReport,
+};
+

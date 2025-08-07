79ff52fce11d52e995070a6a331dfc27ddf6bf1e
Glad99 <gladysalu1@gmail.com>
2025-08-07 18:08:26 +0100

    commit

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../assets/components/layouts/AuthLayout'
+import Input from '../../assets/components/layouts/Inputs/input';
+import { Link, useNavigate } from 'react-router-dom';
 
-const login = () => {
-  return (
-    <div>
-      <AuthLayout>login</AuthLayout>
-    </div>
-  )
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('null');
+
+  const navigate = useNavigate();
+
+  //Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+  return <AuthLayout>
+        <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+          <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+          <p className="text-xs text-slate-700 mt-[5px] mb-6 ">Please enter your details to log in</p>
+
+          <form onSubmit={handleLogin}>
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="Enter your email"
+              type="text"
+            />
+
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="Min 8 Characters"
+              type="password"
+            />
+
+            {error && <p className="text-red-500 text-xs mt-2.5">{error}</p>}
+
+            <button
+              type="submit"
+              className="btn-primary">
+                LOGIN
+              </button>
+
+              <p className="text-[13px] text-slate-800 mt-3">
+                Don't have an account? {""}
+                <Link to="/signup" className="text-primary font-medium underline">
+                  SignUp
+                </Link>
+              </p>
+          </form>
+        </div>
+      </AuthLayout>;
 }
 
-export default login
+export default Login

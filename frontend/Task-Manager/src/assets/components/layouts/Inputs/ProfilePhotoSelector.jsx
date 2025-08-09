8780d3130cd1d4c359ce6715a812d83b143ba6b3
Glad99 <gladysalu1@gmail.com>
2025-08-09 23:51:39 +0100

    commit

--- frontend/Task-Manager/src/assets/components/layouts/Inputs/ProfilePhotoSelector.jsx
+++ frontend/Task-Manager/src/assets/components/layouts/Inputs/ProfilePhotoSelector.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { LuUser, LuUpload, LuTrash } from 'react-icons/lu'
 
 const ProfilePhotoSelector = ({image, setImage}) => {
+    const inputRef = useRef(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file) {
+            // Update the image state
+            setImage(file);
+
+            // Generate a preview URL from the file
+            const preview = URL.createObjectURL(file);
+            setPreviewUrl(preview);
+        }
+    };
+
+    const
   return (
     <div>
       
